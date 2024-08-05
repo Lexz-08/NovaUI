26b4311dc6e67cab472f4c86a8e7f47024d54119
Lexz-08 <sh4d0w4rch3r415@gmail.com>
2024-08-05 02:35:20 -0700

    added selection rule designers

---
+++ NovaUI/Helpers/Designers/LengthResizeDesigner.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms.Design;
+
+namespace NovaUI.Helpers.Designers
+{
+	internal class LengthResizeDesigner : ControlDesigner
+	{
+		public override SelectionRules SelectionRules => SelectionRules.Moveable | SelectionRules.LeftSizeable | SelectionRules.RightSizeable;
+	}
+}
---
+++ NovaUI/Helpers/Designers/NoResizeDesigner.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms.Design;
+
+namespace NovaUI.Helpers.Designers
+{
+	internal class NoResizeDesigner : ControlDesigner
+	{
+		public override SelectionRules SelectionRules => SelectionRules.Moveable;
+	}
+}
